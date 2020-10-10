11bd854e1512b8f14f427407f7f6af9dab6b4b99
Yusuf Bıyık <yusufbyk3660@gmail.com>
2020-10-10 16:18:41 +0300

    🗑 Delete items
    Items are deleted

--- src/app/model.ts
+++ src/app/model.ts
@@ -2,12 +2,7 @@ export class Model {
     items;
 
     constructor() {
-        this.items = [
-            new TodoItem("High","Online Derslere Katıl",false),
-            new TodoItem("Medium","Freelance Projelerde İlerleme Kaydet",false),
-            new TodoItem("High","Ders Çalış",false),
-            new TodoItem("High","Tekrar Yap",false)
-        ];
+        this.items = [];
     }
 }
 
